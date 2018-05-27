76c832c4baba0f8dbc15fc99bbeec8753b109fcd
MarkusJx <markus.jaeger1439@gmail.com>
2018-05-27 18:13:21 +0200

    Update display_log.sh

--- scripts/display_log.sh
+++ scripts/display_log.sh
@@ -1,8 +1,19 @@
 #!/bin/bash/
 
-# Output the log file
-echo -e "$(cat /home/pi/installscript_out.log)"
-
-# Delete files
-sudo rm /home/pi/display_log
-sudo rm /home/pi/installscript_out.log
+# Check if installation is done
+File=installscript_out.log
+if grep -q 1 "$File";
+then
+        # Output the log file
+        echo -e "$(cat /home/pi/installscript_out.log)"
+        
+        # Delete files
+        sudo rm /home/pi/display_log
+        sudo rm /home/pi/installscript_out.log
+else
+        # Output the log file
+        echo -e "$(cat /home/pi/installscript_out.log)"
+        
+        # Send the current status to the User
+        tail -n0 -f installscript_out.log
+fi
