c76a8563acf1d8c85d3a78d42bebfdbf3bc8b59f
MarkusJx <markus.jaeger1439@gmail.com>
2018-05-27 21:48:07 +0200

    Update script_part_2.sh

--- scripts/script_part_2.sh
+++ scripts/script_part_2.sh
@@ -88,8 +88,10 @@ sudo service php7.0-fpm reload
 # Install phpmyadmin
 echo -e "${CYAN}Installing phpmyadmin...$NC"
 export DEBIAN_FRONTEND=noninteractive
-sudo yes | apt-get install phpmyadmin
-sudo cp /tmp/phpmyadmin.conf /etc/dbconfig-common/phpmyadmin.conf
+sudo apt-get install phpmyadmin -y -q
+sudo sh -c "echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections"
+sudo sh -c "echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect lighttpd' | debconf-set-selections"
+#sudo cp /tmp/phpmyadmin.conf /etc/dbconfig-common/phpmyadmin.conf
 echo -e "${CYAN}Running dpkg-reconfigure on phpmyadmin...$NC"
 dpkg-reconfigure --frontend=noninteractive phpmyadmin
 
