49821460ef0492989d592a83308c02ee7cbc77e2
MarkusJx <markus.jaeger1439@gmail.com>
2018-05-27 16:27:46 +0200

    Update installscript.sh

--- scripts/installscript.sh
+++ scripts/installscript.sh
@@ -1,32 +1,45 @@
 #!/bin/bash/
 
-GREEN='\033[0;32m'
+GREEN='\033[1;32m'
 NC='\033[0m'
+CYAN='\033[1;36m'
 
 # Download part 2 of this script
+echo -e "${CYAN}Downloading second part of this script...$NC"
 sudo wget -q https://git.io/vpNyW -O /home/pi/script_part_2
 
 #Download the phpmyadmin.conf
+echo -e "${CYAN}Downloading phpmyadmin.conf...$NC"
 sudo wget -q https://git.io/vhmUM -O /home/pi/phpmyadmin.conf
 
 # Add part 2 of this script to startup
+echo -e "${CYAN}Adding second part to startup...$NC"
 (crontab -l ; echo "@reboot bash /home/pi/script_part_2 >> /tmp/installscript_out.log 2>&1") | crontab -
-echo -e "${GREEN}Succesfully added the second part of this script to startup!${NC}"
+echo -e "${GREEN}Succesfully added the second part of this script to startup!$NC"
+echo -e "${CYAN}Active Cron Jobs:$NC"
+sudo crontab -e
+echo -e "\n"
 
 # Update
+echo -e "${CYAN}Updating sources...$NC"
 sudo apt update
 
 # Upgrade
+echo -e "${CYAN}Upgrading...$NC"
 sudo apt upgrade -y
 
 # Remove unnecessary Packages
+echo -e "${CYAN}Running apt-autoremove...$NC"
 sudo apt autoremove -y
 
 # Install Git
+echo -e "${CYAN}Installing Git...$NC"
 sudo apt install git -y
 
 # Install RaspAp
-wget -q https://git.io/voEUQ -O /tmp/raspap && sudo bash /tmp/raspap
+echo -e "${CYAN}Installing Raspap...$NC"
+wget -q https://git.io/voEUQ -O /tmp/raspap && yes y | sudo bash /tmp/raspap
 
 # Reboot
+echo -e "${CYAN} $NC"
 sudo reboot now
