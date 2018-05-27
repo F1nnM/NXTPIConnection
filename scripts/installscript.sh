c9ee0acc9465ffb4a5c574320eab7d0f27aadabb
MarkusJx <markus.jaeger1439@gmail.com>
2018-05-27 15:23:12 +0200

    Update installscript.sh

--- scripts/installscript.sh
+++ scripts/installscript.sh
@@ -4,13 +4,13 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 # Download part 2 of this script
-wget -q https://git.io/vpNyW -O /tmp/script_part_2
+wget -q https://git.io/vpNyW -O script_part_2
 
 #Download the phpmyadmin.conf
-wget -q https://git.io/vhmUM -O /tmp/phpmyadmin.conf
+wget -q https://git.io/vhmUM -O phpmyadmin.conf
 
 # Add part 2 of this script to startup
-(crontab -l ; echo "@reboot bash /tmp/script_part_2 >> /tmp/installscript_out.log 2>&1") | crontab -
+(crontab -l ; echo "@reboot bash script_part_2 >> /tmp/installscript_out.log 2>&1") | crontab -
 echo -e "${GREEN}Succesfully added the second part of this script to startup!${NC}"
 
 # Update
