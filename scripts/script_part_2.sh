7b93fdddd68d205eda788d6dbf1a0009e84c2154
MarkusJx <markus.jaeger1439@gmail.com>
2018-05-27 16:40:33 +0200

    Update script_part_2.sh

--- scripts/script_part_2.sh
+++ scripts/script_part_2.sh
@@ -1,39 +1,53 @@
 #!/bin/bash/
 
+GREEN='\033[1;32m'
+NC='\033[0m'
+CYAN='\033[1;36m'
+
 # Wait for Network
 while ! ping -c 1 -W 1 8.8.8.8; do
-    echo "Waiting for 8.8.8.8 - network interface might be down..."
+    echo -e "${CYAN}Waiting for 8.8.8.8 - network interface might be down...$NC"
     sleep 1
 done
 
 # Remove this script from startup
+echo -e "${CYAN}Removing this script from startup...$NC"
 sudo crontab -l | grep -v "@reboot bash /home/pi/script_part_2 >> /tmp/installscript_out.log 2>&1" | crontab -
 
 # Create Directory for RaspAp
+echo -e "${CYAN}Creating Directory for RaspAp...$NC"
 sudo mkdir /var/www/html/raspap
 
 # move RaspAp to Directory
+echo -e "${CYAN}Moving RaspAp...$NC"
 sudo mv /var/www/html/* /var/www/html/raspap/
 
 # Create Directory for GumCP
+echo -e "${CYAN}Creating Directory for GumCP$NC"
 sudo mkdir /var/www/html/admin
 
 # Clone GumCP
+echo -e "${CYAN}Cloning GumCP...$NC"
 sudo git clone https://github.com/gumslone/GumCP.git /var/www/html/admin/
 
 # Install PHP 7.0 and mariadb
+echo -e "${CYAN}Installing PHP 7.0 and mariadb...$NC"
 sudo apt install php7.0-fpm php7.0 php-ssh2 php-cgi mariadb-server mariadb-client -y
 
 # Replace line in php-fpm config
+echo -e "${CYAN}Uncommenting line in php config file...$NC"
 sudo sed -i -e 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=1/g' /etc/php/7.0/fpm/php.ini
 
 # Create Backup of fastcgi-php.conf
+echo -e "${CYAN}Creating Backup of fastcgi-php.conf...$NC"
 sudo cp /etc/lighttpd/conf-available/15-fastcgi-php.conf /etc/lighttpd/conf-available/15-fastcgi-php.conf.bak
 
 # Delete fastcgi-php.conf
+echo -e "${CYAN}Deleting fastcgi-php.conf...$NC"
 sudo rm /etc/lighttpd/conf-available/15-fastcgi-php.conf
 
 # Write fastcgi-php.conf config file
+echo -e "${CYAN}Writing new fastcgi-php.conf...$NC"
 sudo sh -c 'echo "# /usr/share/doc/lighttpd-doc/fastcgi.txt.gz" >> /etc/lighttpd/conf-available/15-fastcgi-php.conf'
 sudo sh -c 'echo "# http://redmine.lighttpd.net/projects/lighttpd/wiki/Docs:ConfigurationOptions#mod_fastcgi-fastcgi" >> /etc/lighttpd/conf-available/15-fastcgi-php.conf'
 sudo sh -c 'echo "" >> /etc/lighttpd/conf-available/15-fastcgi-php.conf'
@@ -46,6 +60,7 @@ sudo sh -c 'echo "        ))" >> /etc/lighttpd/conf-available/15-fastcgi-php.con
 sudo sh -c 'echo ")" >> /etc/lighttpd/conf-available/15-fastcgi-php.conf'
 
 # Enable Lighttpd mods fastcgi and fastcgi-php
+echo -e "${CYAN}Enabling Lighttpd mods...$NC"
 sudo lighttpd-enable-mod fastcgi
 sudo lighttpd-enable-mod fastcgi-php
 
@@ -53,32 +68,42 @@ sudo lighttpd-enable-mod fastcgi-php
 sudo ls -l /etc/lighttpd/conf-enabled
 
 # Reload lighttpd
+echo -e "${CYAN}Reloading lighttpd$NC"
 sudo service lighttpd force-reload
 
 # Install php packages
+echo -e "${CYAN}Installing php packages...$NC"
 sudo apt install php7.0-mysql php7.0-curl php7.0-gd php7.0-intl php-pear php-imagick php7.0-imap php7.0-mcrypt php-memcache php7.0-pspell php7.0-recode php7.0-sqlite3 php7.0-tidy php7.0-xmlrpc php7.0-xsl php7.0-mbstring php-gettext php-apcu -y
 
 # Reload php7.0-fpm
+echo -e "${CYAN}Reloading php7.0-fpm...$NC"
 sudo service php7.0-fpm reload
 
 # Install phpmyadmin
+echo -e "${CYAN}Installing phpmyadmin...$NC"
 export DEBIAN_FRONTEND=noninteractive
 sudo apt install phpmyadmin -y
 sudo cp /home/pi/phpmyadmin.conf /etc/dbconfig-common/phpmyadmin.conf
+echo -e "${CYAN}Running dpkg-reconfigure on phpmyadmin$NC"
 dpkg-reconfigure --frontend=noninteractive phpmyadmin
 
 # Reload lighttpd
+echo -e "${CYAN}Reloading lighttpd...$NC"
 sudo /etc/init.d/lighttpd force-reload
 
 # Install phpmyadmin second time, because first time installation always fails
+echo -e "${CYAN}Trying to Install phpmyadmin second time, if first time installation fails...$NC"
 sudo apt install phpmyadmin -y
 
 # Install Java
+echo -e "${CYAN}Installing Java...$NC"
 sudo apt install oracle-java8-jdk -y
 
 # Delete everything
+echo -e "${CYAN}Deleting this script...$NC"
 sudo rm /home/pi/phpmyadmin.conf
 sudo rm /home/pi/script_part_2
 
 # Install BrickPi drivers
+echo -e "${CYAN}Installing BrickPi drivers...$NC"
 sudo curl -kL dexterindustries.com/update_brickpi_plus | bash
