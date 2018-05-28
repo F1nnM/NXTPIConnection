e10b4f180635e2cc801339dd7e5866c6a831dbb5
MarkusJx <markus.jaeger1439@gmail.com>
2018-05-28 16:35:24 +0200

    Added option to delete User

--- scripts/createmariadbuser.sh
+++ scripts/createmariadbuser.sh
@@ -11,23 +11,54 @@ if ! hash mariadb 2>/dev/null; then
     exit 1
 fi
 
-echo "Enter Username of new User: "
-read Username
-
-echo "Enter Password: "
-read Password
-
-sudo mariadb -e "CREATE USER '$Username'@'localhost' IDENTIFIED BY '$Password';"
-
-echo "Grant all Privileges to new User? [Y/N]: "
-read answer
-declare -l answer
-answer=$answer
-if [ $answer == "y" ] || [ $answer == "yes" ] ; then
-        sudo mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '$Username'@'localhost' WITH GRANT OPTION;"
+echo "Select what to do:"
+echo "[1] Create User"
+echo "[2] Delete User"
+read number
+echo "\n"
+if [ $number == "1" ] ; then
+    create_user
+else if [ $number == "2" ] ; then
+    delete_user
+else
+    echo -e "${RED}Wrong Arguments! Type in 1 or 2!$NC"
+    exit 1
 fi
 
-sudo mariadb -e "FLUSH PRIVILEGES;"
+bye
 
-echo -e "${GREEN}Done.$NC"
-echo "Bye."
+function create_user(){
+    echo "Enter Username of new User: "
+    read Username
+
+    echo "Enter Password: "
+    read Password
+
+    sudo mariadb -e "CREATE USER '$Username'@'localhost' IDENTIFIED BY '$Password';"
+
+    echo "Grant all Privileges to new User? [Y/N]: "
+    read answer
+    declare -l answer
+    answer=$answer
+    if [ $answer == "y" ] || [ $answer == "yes" ] ; then
+        sudo mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '$Username'@'localhost' WITH GRANT OPTION;"
+    fi
+
+    sudo mariadb -e "FLUSH PRIVILEGES;"
+}
+
+function delete_user(){
+    echo "Enter Username to delete:"
+    read Username
+
+    sudo mariadb -e "EXEC sp_MSForEachDB 'USE [?]; IF  EXISTS (SELECT * FROM sys.schemas WHERE name = N''$Username'') DROP SCHEMA [$Username]; '"
+    
+    sudo mariadb -e "EXEC sp_MSForEachDB 'USE [?]; IF  EXISTS (SELECT * FROM sys.database_principals WHERE name = N''EU\USUBOLS'') DROP USER [$Username]; '"
+
+    sudo mariadb -e "IF  EXISTS (SELECT * FROM sys.server_principals WHERE name = N'$Username') DROP LOGIN [$Username];"
+}
+
+function bye(){
+    echo -e "${GREEN}Done.$NC"
+    echo "Bye."
+}
