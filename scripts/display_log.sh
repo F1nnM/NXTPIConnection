62510fe401420bf5dfd64c9a04e8673086383885
MarkusJx <markus.jaeger1439@gmail.com>
2018-06-04 19:46:33 +0200

    Update display_log.sh

--- scripts/display_log.sh
+++ scripts/display_log.sh
@@ -5,7 +5,7 @@ File=/home/pi/NXTPi/installscript_out.log
 if grep -q Instinfo:DONE. "$File";
 then
         # Output the log file
-        echo -e "$(cat /home/pi/NXTPi/installscript_out.log)"
+        echo -e "$(cat /home/pi/NXTPi/install/installscript_out.log)"
         echo -e "\n\n\n\n"
 
         # Ask for deleting the installation files
@@ -16,6 +16,7 @@ then
         if [ $answer == "y" ] || [ $answer == "yes" ] ; then
                 # Delete files
                 echo "Deleting all installation files..."
+                sudo rm -R -v /home/pi/NXTPi/install/*
                 sudo rm -R -v /home/pi/Dexter
                 sudo rm -v /home/pi/display_log
         fi
