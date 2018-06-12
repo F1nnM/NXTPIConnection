7761531938e92c9b9fc53c78b64ccc9e6943a33e
MarkusJx <markus.jaeger1439@gmail.com>
2018-06-12 20:55:43 +0200

    Updated NXTPi.sh
    added option to compile .nxj files;
    added help;
    moved some code;
    replaced some parts of the code;

--- scripts/nxtpi.sh
+++ scripts/nxtpi.sh
@@ -1,25 +1,70 @@
 #!/bin/bash
 
-function main() {
-    if [[ "$1" == "run" ]] ; then
-        filename=$2
-        if [[ ${filename: -4} == ".jar" ]] ; then
-            mainclassmf=$(unzip -c $filename META-INF/MANIFEST.MF | grep Main-Class)
-            mainclass=$(cut -d' ' -f2 <<< $mainclassmf)
-            mainclass=$(sed -e "s/[^.a-zA-ZäÄüÜöÖ0-9_$]//g" <<< $mainclass)
-            echo "Path to main class: $mainclass"
-            jarargs=${3#${1}}
-            jarargs=${jarargs#${2}}
-            sudo java -d32 -cp "$filename:/opt/leJOS_NXJ_0.9.1beta-3/lib/pc/*" $mainclass $jarargs
-        else
-            echo "The given file is no jar file!"
-        fi
+function run() {
+    filename=$2
+    if [[ ${filename: -4} == ".jar" ]] ; then
+        mainclassmf=$(unzip -c $filename META-INF/MANIFEST.MF | grep Main-Class)
+        mainclass=$(cut -d' ' -f2 <<< $mainclassmf)
+        mainclass=$(sed -e "s/[^.a-zA-ZäÄüÜöÖ0-9_$]//g" <<< $mainclass)
+        echo "Path to main class: $mainclass"
+        jarargs=${@#${1}}
+        jarargs=${jarargs#${2}}
+        sudo java -d32 -cp "$filename:/opt/leJOS_NXJ_0.9.1beta-3/lib/pc/*" $mainclass $jarargs
     else
-        echo "Wrong Arguments. Usage: nxtpi run [jarfile]"
+        echo "The given file is no jar file!"
     fi
 }
 
-args=${@#${1}}
-args=${@#${2}}
+function getJavaFiles() {
+    if [[ "$1" != */ ]] ; then
+        dir="$1/"
+    fi
 
-main "$1" "$2" "$args"
+    files=()
+
+    for file in $dir*; do
+        [ -e "$file" ] || continue
+        if [[ ${file: -5} == ".java" ]] ; then
+            files=(${files[@]} "$file")
+        elif [[ -d $file ]] ; then
+            files=(${files[@]} $(getJavaFiles $file))
+        fi
+    done
+
+    echo ${files[@]}
+}
+
+function nxj() {
+    if [ -f $2 ] && [ ${2: -4} == ".jar" ] ; then
+            jarargs=${@#${1}}
+	    jarargs=${jarargs#${2}}
+            jarargs=${jarargs#${3}}
+            jarargs=${jarargs#${4}}
+            name=${2::-4}
+            name="$name.nxj"
+            nxjc -bootclasspath "$2" -cp "/opt/leJOS_NXJ_0.9.1beta-3/lib/nxt/classes.jar" $(getJavaFiles $3)
+            nxjlink -cp "$2" -o "$name" $4 #$jarargs WIP
+            nxjupload "$name"
+    fi
+}
+
+function help() {
+    echo "------------------nxtpi help------------------"
+    echo "General usage: nxtpi [-jar or -nxj] <args>"
+    echo "To run a leJos PC project: nxtpi -pc [path-to-jarfile] <jarargs>"
+    echo "To run a leJos NXJ project: nxtpi -nxj [path-to-jarfile] [path-to-source-folder] [main-class] <java-args>"
+}
+
+function main() {
+    if [[ "$1" == "-pc" ]] ; then
+        run "$@"
+    elif [[ "$1" == "-nxj" ]] ; then
+        nxj "$@"
+    elif [[ "$1" == "-help" ]] ; then
+        help
+    else
+        echo "Illegal Arguments! Usage: nxtpi [-jar or -nxj] <args>. For further help type \"nxtpi -help\""
+    fi
+}
+
+main $@
